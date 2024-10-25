bb02e27ad42c915682f572058db9e35c701349af
Newton Duarte <newton_@hotmail.com>
2024-10-25 14:41:43 -0300

    feat: add count provider and hook to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { CountProvider } from './modules/count/context/count-context-provider'
+import { useCountStore } from './modules/count/hooks/useCountStore'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <CountProvider initialCount={3}>
+      <Component />
+    </CountProvider>
   )
 }
 
-export default App
+function Component() {
+  const { count, increment } = useCountStore((state) => state)
+
+  return (
+    <>
+      <h1>Vite + React + Zustand</h1>
+      <div className="card">
+        <button onClick={increment}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
